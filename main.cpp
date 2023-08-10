58af6cd7041fa5f09c10af59c70f3db09425768f
runner-1 <jrvollmer@wisc.edu>
2023-08-10 04:04:17 +0000

    Update main.cpp for workflow control and debugging
    Add a message to signal the end of the job. Also, blink an LED to make
    debugging/determining whether or not the code was flashed easier.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "mbed.h"
 #include "monitor.h"
 
+#define BLINKING_RATE     2000ms
 // Main code. Should initialize devices and then transform into the CAN loop
 
 int main()
@@ -17,8 +18,21 @@ int main()
     //initCAN(125000);
     //monitor_initMonitor(300ms);
 
+    int iteration = 0;
+
+#ifdef LED1
+    DigitalOut led(LED1);
+#else
+    bool led;
+#endif
+
     while(1)
     {
+        // LED blink
+        led = !led;
+        printf("Blink num %d\n", iteration);
+
+        // Stripped down MCC
         /*if(!READ_BIT_POS(curGPIO, POWER_BIT))
         {
             drive_setTargetVelocity(0);
@@ -28,8 +42,15 @@ int main()
         {
             drive_overrideAccel(calculate_pedal_press(analog_getCurPedal()));
         }*/
-		printf("Heyooo\n");
-        wait_us(SEC_TO_USEC(.75));
+
+        // TODO printf("Heyooo %d\n", iteration);
+        if(iteration >= 10)
+            printf("Done\n");
+        // TODO wait_us(SEC_TO_USEC(.75));
+        iteration ++;
+
+        // LED blink wait
+        ThisThread::sleep_for(BLINKING_RATE);
     }
 
     // Enter send loop
