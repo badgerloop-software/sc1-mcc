5442eba6d2da2f261e29600cdff35b8150f0b01b
Wilson Guo <wigu701@gmail.com>
2023-10-08 14:18:19 -0500

    Fix interrupt polling and telemetry file

--- src/analog.cpp
+++ src/analog.cpp
@@ -3,26 +3,28 @@
 // initialize Ticker to setup a recurring interrupt to repeatedly call the function at a specified rate
 Ticker readAnalogDelay;
 
+// Disable mutex usage of analog read for interrupt polling
+// because mutex cannot be used in ISR
+class AnalogInMutexless : public AnalogIn {
+public:
+    AnalogInMutexless(PinName pin) : AnalogIn(pin) { }
+    virtual void lock() { }
+    virtual void unlock() { }
+};
+
 // assign analog inputs to the correct pins
-AnalogIn acceleratorPedalPin(A6);
-AnalogIn acceleratorSignalPin(A3);
-AnalogIn brakeStatusPin(A1);
-AnalogIn regenerativeBrakingPin(A4);
+AnalogInMutexless acceleratorPedalPin(A6);
+AnalogInMutexless brakeStatusPin(A1);
+AnalogInMutexless regenerativeBrakingPin(A4);
 
 // initialize the analog inputs' readings
 volatile float acceleratorPedal = 0;
-volatile float acceleratorSignal = 0;
 volatile float brakeStatus = 0;
 volatile float regenerativeBraking = 0;
 
 // read accelerator pedal input
 void readAcceleratorPedal(){ 
     acceleratorPedal = acceleratorPedalPin.read_voltage();
-}
-
-// read accelerator signal input
-void readAcceleratorSignal(){
-    acceleratorSignal = acceleratorSignalPin.read_voltage();
 }
 
 // read brake status input
@@ -38,21 +40,15 @@ void readRegeativeBraking(){
 // read all analog input
 void readAnalog(){
     readAcceleratorPedal();
-    readAcceleratorSignal();
     readBrakeStatus();
     readRegeativeBraking();
 }
 
-
-
-
-// repeatedly call the function with the given parameter
+// Set up polling of analog IO at specified rate
 void initAnalog(std::chrono::microseconds readSignalPeriod) {
-
     acceleratorPedalPin.set_reference_voltage(refrence_voltage);
-    acceleratorSignalPin.set_reference_voltage(refrence_voltage);
     brakeStatusPin.set_reference_voltage(refrence_voltage);
     regenerativeBrakingPin.set_reference_voltage(refrence_voltage);
 
-    readAnalogDelay.attach(&readAnalog, readSignalPeriod);
+    readAnalogDelay.attach(readAnalog, readSignalPeriod);
 }
