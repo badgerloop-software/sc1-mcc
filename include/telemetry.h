45f912ae4676fd68fc7bb1fd2b603e709204aec2
jwang2672 laptop <jwang2672@wisc.edu>
2023-10-13 19:41:45 -0500

    Improve cruise control IO readings (Speed only)

--- include/telemetry.h
+++ include/telemetry.h
@@ -4,18 +4,27 @@
 #include "mbed.h"
 
 // Bit field to store digital IO
-struct Digital_Data{
-    bool setCruiseControl:1;
-    bool resetCruiseControl:1;
-    bool cruisePowerMode:1;
-    bool cruiseSpeedMode:1;
-    bool motorPower:1;
-    bool forwardAndReverse:1;
-    bool ecoMode:1;
+struct Digital_Data {
+  bool cruiseEnabled : 1;
+  bool motorPower : 1;
+  bool forwardAndReverse : 1;
+  bool ecoMode : 1;
 };
+
+enum class CRUZ_MODE {
+  OFF,
+  SPEED,
+  POWER,
+};
+
+//TODO: Tune values based on testing
+#define BRAKE_THRESHOLD 69.0
+#define INCREMENT_RPM   21
+#define INCREMENT_POWER 420
 
 // Variables to store readings
 extern volatile struct Digital_Data digital_data;
+extern volatile CRUZ_MODE cruzMode;
 
 extern volatile float rpm;
 extern volatile float mph;
@@ -24,5 +33,6 @@ extern volatile float acceleratorPedal;
 extern volatile float brakeStatus;
 extern volatile float regenerativeBraking;
 
+extern volatile float motorSpeedSetpoint;
 
 #endif
