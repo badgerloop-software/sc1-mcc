3bde2b4a797088bcbfaccc9a3349648bd8e9c5b2
jwang2672 laptop <jwang2672@wisc.edu>
2023-10-13 19:41:45 -0500

    Improve cruise control IO readings (Speed only)

--- src/digital.cpp
+++ src/digital.cpp
@@ -1,6 +1,7 @@
 #include "digital.h"
 
-// initialize Ticker to setup a recurring interrupt to repeatedly call the function at a specified rate
+// initialize Ticker to setup a recurring interrupt to repeatedly call the
+// function at a specified rate
 Ticker readDigitalDelay;
 
 // assign digital input to the correct pins
@@ -15,52 +16,84 @@ DigitalIn ecoModePin(D11);
 
 // storing every digital data inside a struct to reduce the memory
 volatile struct Digital_Data digital_data = {};
+volatile CRUZ_MODE cruzMode = CRUZ_MODE::OFF;
+volatile float motorSpeedSetpoint = 0;
 
+void readCruiseControl() {
+  // read set cruise control input
+  // increments
+  bool setCruz = setCruiseControlPin.read();
 
-// read set cruise control input
-void readSetCruiseControl(){
-    digital_data.setCruiseControl = setCruiseControlPin.read();
+  // read reset cruise control input
+  // decrment
+  bool resetCruz = resetCruiseControlPin.read();
+
+  // read cruise power mode input
+  bool cruisePowerMode = cruisePowerModePin.read();
+
+  // read cruise speed mode input
+  bool cruiseSpeedMode = cruiseSpeedModePin.read();
+
+  float increment_by = 0;
+
+  // set cruise control mode and set what to increment by
+  if (cruisePowerMode) {
+    cruzMode = CRUZ_MODE::POWER;
+    increment_by = INCREMENT_POWER;
+  } else if (cruiseSpeedMode) {
+    cruzMode = CRUZ_MODE::SPEED;
+    increment_by = INCREMENT_RPM;
+  } else {
+    cruzMode = CRUZ_MODE::OFF;
+  }
+
+  // if mode is off or break is pressed, cruise control is off (and can't be
+  // turn on)
+  if (cruzMode == CRUZ_MODE::OFF || brakeStatus > BRAKE_THRESHOLD) {
+    digital_data.cruiseEnabled = false;
+    return;
+  }
+
+  /*---cruise control can be turn on---*/
+
+  // if cruise control is on, make reset and set decrement and increment
+  if (digital_data.cruiseEnabled) {
+    // increments
+    if (setCruz) {
+      motorSpeedSetpoint += increment_by;
+    }
+    // decrement
+    if (resetCruz) {
+      motorSpeedSetpoint -= increment_by;
+    }
+  } else {
+    // enable cruise control if set cruz or reset is pressed
+    if (setCruz) {
+      digital_data.cruiseEnabled = true;
+      motorSpeedSetpoint = rpm;
+    }
+    if (resetCruz) {
+      digital_data.cruiseEnabled = true;
+    }
+  }
 }
 
-// read reset cruise control input
-void readResetCruiseControl(){
-    digital_data.resetCruiseControl = resetCruiseControlPin.read();
-}
-
-// read cruise power mode input
-void readCruisePowerMode(){
-    digital_data.cruisePowerMode = cruisePowerModePin.read();
-}
-
-// read cruise speed mode input
-void readCruiseSpeedMode(){
-    digital_data.cruiseSpeedMode = cruiseSpeedModePin.read();
-}
 
 // read motor power input
-void readMotorPower(){
-    digital_data.motorPower = motorPowerPin.read();
-}
+void readMotorPower() { digital_data.motorPower = motorPowerPin.read(); }
 
 // read forward and reverse input
-void readForwardAndReverse(){
-    digital_data.forwardAndReverse = forwardAndReversePin.read();
-}
+void readForwardAndReverse() { digital_data.forwardAndReverse = forwardAndReversePin.read(); }
 
 // read eco mode input
-void readEcoMode(){
-    digital_data.ecoMode = ecoModePin.read();
-}
+void readEcoMode() { digital_data.ecoMode = ecoModePin.read(); }
 
 // read all the digital inputs
-void readDigital(){
-    readSetCruiseControl();
-    readResetCruiseControl();
-    readCruisePowerMode();
-    readCruiseSpeedMode();
-    readMotorPower();
-    readForwardAndReverse();
-    readEcoMode();
+void readDigital() {
+  readCruiseControl();
+  readMotorPower();
+  readForwardAndReverse();
+  readEcoMode();
 }
 
 // Set up polling of digital IO at specified rate
