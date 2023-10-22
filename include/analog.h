53768daefb65ca53635c6346ed47f2cf79b87925
Devansh Agrawal <agrawaldev14@gmail.com>
2023-10-22 11:23:15 -0500

    State Machine and Cruise Control Code

--- include/analog.h
+++ include/analog.h
@@ -6,6 +6,9 @@
 // defining the reference voltage for analog inputs
 #define refrence_voltage 3.3
 
+#define PEDAL_ON_THRESHOLD 0.25 // 1.5V/5V
+#define PEDAL_RANGE 0.417  // 0.86 - 0.3, where 0.86 = 4.33V/5V is the max
+
 // read all the analog inputs
 void readAnalog();
 
--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@
 #include "analog.h"
 #include "speed.h"
 #include "motor_control.h"
+#include "canmcc.h"
 
-#define SPEED_CALC_INTERVAL 10ms
-#define ANALOG_CALC_INTERVAL 10000us
-#define DIGITAL_CALC_INTERVAL 10000us
+#define SPEED_CALC_INTERVAL 25ms
+#define ANALOG_CALC_INTERVAL 50000us
+#define DIGITAL_CALC_INTERVAL 50000us
 
 // time between checking for transitions in state machine in us
-#define SM_TRANSITION_INTERVAL 1000000us
+#define SM_TRANSITION_INTERVAL 500000us
+
+#define CAN_RX PA_11
+#define CAN_TX PA_12
 
 
 int main()
 {
+    // Set terminal to fast print
+    BufferedSerial terminal(USBTX, USBRX, 115200);
+
     // read RPM and MPH every 0.01 second
     startSpeedCalculation(SPEED_CALC_INTERVAL);
 
@@ -27,7 +34,63 @@ int main()
     // initialize state machine
     MCCState state_machine(SM_TRANSITION_INTERVAL);
 
-    while (true) {
+    CANMCC canBus(CAN_RX, CAN_TX, &state_machine);
 
+
+    while(true){
+        printf("\e[1;1H\e[2J");
+        //state printout
+        switch (state_machine.get_state()) {
+            case MCCStates::OFF :
+                printf("State: OFF\n");
+                break;
+            case MCCStates::PARK :
+                printf("State: PARK\n");
+                break;
+            case MCCStates::IDLE :
+                printf("State: IDLE\n");
+                break;
+            case MCCStates::FORWARD :
+                printf("State: FORWARD\n");
+                break;
+            case MCCStates::REVERSE :
+                printf("State: REVERSE\n");
+                break;
+            case MCCStates::CRUISE_POWER :
+                printf("State: CRUISE\n");
+                break;
+            case MCCStates::CRUISE_SPEED:
+                printf("State: CRUISE\n");
+                break;
+            default:
+                printf("Unknown state?\n");
+                break;
+        }
+
+        // inputs
+        printf("INPUTS-------------------------------\n");
+        printf("Motor Power: %s\n", digital_data.motorPower ? "On" : "Off");
+        printf("Forward and Reverse: %s\n", digital_data.forwardAndReverse ? "Forward" : "Reverse");
+        printf("RPM: %f\n", rpm);
+        printf("MPH: %f\n", mph);
+        printf("Brake Status: %f\n", brakeStatus);
+        printf("acceleratorPedal: %f\n", acceleratorPedal);
+        switch(cruzMode) {
+            case CRUZ_MODE::OFF :
+                printf("cruzMode: OFF\n");
+                break;
+            case CRUZ_MODE::POWER :
+                printf("cruzMode: POWER\n");
+                break;
+            case CRUZ_MODE::SPEED :
+                printf("cruzMode: SPEED\n");
+                break;
+        }
+        printf("motorSpeedSetpoint: %f\n", motorSpeedSetpoint);
+        printf("parkingBrake: %s\n", parkBrake ? "On" : "Off");
+        canBus.send_mcc_data();
+        canBus.doRead(1000ms);
+
+        wait_us(1000000);
     }
 }
