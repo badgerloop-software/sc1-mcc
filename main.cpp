928028fc648e4a8ed7c2a964dea0a5adfc51575d
Wilson Guo <wigu701@gmail.com>
2024-01-31 23:18:39 -0600

    RPM sim draft

--- main.cpp
+++ main.cpp
@@ -1,39 +1,43 @@
 #include "mbed.h"
-#include "telemetry.h"
-#include "digital.h"
-#include "analog.h"
-#include "speed.h"
-#include "motor_control.h"
-#include "motorError.h"
 
-#define SPEED_CALC_INTERVAL 10ms
-#define ANALOG_CALC_INTERVAL 10000us
-#define DIGITAL_CALC_INTERVAL 10000us
 
-// time between checking for transitions in state machine in us
-#define SM_TRANSITION_INTERVAL 1000000us
+#define PWM_PIN D2
+#define STEP_SIZE 10
 
+
+PwmOut pwmPin(PWM_PIN);
 
 int main()
 {
-    // read RPM and MPH every 0.01 second
-    startSpeedCalculation(SPEED_CALC_INTERVAL);
+    BufferedSerial serial(USBTX, USBRX, 115200);
+    serial.set_blocking(true);
 
-    // read all analog inputs every 0.01 second
-    initAnalog(ANALOG_CALC_INTERVAL);
+    // Fixed 10% duty cycle, the speed depends on frequency
+    pwmPin.write(0.1);
+    pwmPin.period(0);
 
-    // read all digital inputs every 0.01 second
-    initDigital(DIGITAL_CALC_INTERVAL);
-
-    // initialize state machine
-    MCCState state_machine(SM_TRANSITION_INTERVAL);
-
-    startErrorInterpretation();
-
+    int rpm = 0;
+    char buf[1];
     while(true)
     {
-        printf("\nError Type: %s", errorString(errorType));
+        printf("\e[1;1H\e[2J");
+        printf("Simulating RPM: %i", rpm);
+        printf("Press i to increment, d to decrement");
 
-        rtos::ThisThread::sleep_for(1000ms);
+        if (serial.read(buf, 1) > 0) {
+            if (buf[0] == 'i') {
+                rpm += STEP_SIZE;
+            } else if (buf[0] == 'd') {
+                rpm -= STEP_SIZE;
+            }
+
+            if (rpm < 0) rpm = 0;
+        }
+
+        // Divide by 60 to get rps, multiply by 48 to get ticks per second
+        // Period in ms is 1000 / ticks per second
+        // Roughly equivalent to 1333.33333 / rpm
+        float periodMS = 1333.333333 / rpm;
+        pwmPin.period_ms(periodMS);
     }
 }
