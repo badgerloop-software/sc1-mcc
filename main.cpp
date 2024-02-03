b565955d2f2f8b8925a395422fa02a33b687a920
Jacob Alejandro <alejandro2@wisc.edu>
2024-02-02 20:39:30 -0600

    Fix RPM simulator

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include "speed.h"
 
 
 #define PWM_PIN D2
@@ -9,35 +10,47 @@ PwmOut pwmPin(PWM_PIN);
 
 int main()
 {
-    BufferedSerial serial(USBTX, USBRX, 115200);
+    //BufferedSerial serial(USBTX, USBRX, 115200);
+    BufferedSerial serial(USBTX, USBRX);
     serial.set_blocking(true);
 
     // Fixed 10% duty cycle, the speed depends on frequency
     pwmPin.write(0.1);
-    pwmPin.period(0);
+    pwmPin.suspend();
 
-    int rpm = 0;
+    int simRPM = 0;
     char buf[1];
+
     while(true)
     {
+        startSpeedCalculation(20ms);
         printf("\e[1;1H\e[2J");
-        printf("Simulating RPM: %i", rpm);
+        printf("Simulating RPM: %i    ", simRPM);
         printf("Press i to increment, d to decrement");
+        printf("\nActual RPM Calculation: %f", rpm);
 
         if (serial.read(buf, 1) > 0) {
             if (buf[0] == 'i') {
-                rpm += STEP_SIZE;
+                simRPM += STEP_SIZE;
             } else if (buf[0] == 'd') {
-                rpm -= STEP_SIZE;
+                simRPM -= STEP_SIZE;
             }
 
-            if (rpm < 0) rpm = 0;
+            if (simRPM < 0) simRPM = 0;
         }
 
         // Divide by 60 to get rps, multiply by 48 to get ticks per second
         // Period in ms is 1000 / ticks per second
         // Roughly equivalent to 1333.33333 / rpm
-        float periodMS = 1333.333333 / rpm;
+
+        float periodMS;
+        if (simRPM > 0) {
+            pwmPin.resume();
+            periodMS = 1333.333333 / simRPM;
+        } else {
+            pwmPin.suspend();
+        }
+
         pwmPin.period_ms(periodMS);
     }
 }
